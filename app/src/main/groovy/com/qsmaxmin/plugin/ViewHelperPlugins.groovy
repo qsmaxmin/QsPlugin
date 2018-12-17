adebb089962c27dfe9263a9f89eefb394c037193
qsmaxmin <17600662013@163.com>
2018-12-17 13:46:42 +0800

    development

--- app/src/main/groovy/com/qsmaxmin/plugin/ViewHelperPlugins.groovy
+++ app/src/main/groovy/com/qsmaxmin/plugin/ViewHelperPlugins.groovy
@@ -13,11 +13,11 @@ public class ViewHelperPlugins implements Plugin<Project> {
     static final String EXTENSION_NAME = 'QsBase'
 
     @Override
-    void apply(Project target) {
-//        def extension = project.extensions.create(EXTENSION_NAME, ViewHelperExtension)
+    void apply(Project project) {
+        def extension = project.extensions.create(EXTENSION_NAME, ViewHelperExtension)
 
         project.task('QsBaseTask') << {
-            println 'QsBase is enable:'
+            println 'QsBase is enable:'+extension.enable
         }
     }
 }
